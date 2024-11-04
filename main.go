2ee46c61e2bdf66179ad7440cb6cdc8561f9d5a2
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-11-04 16:11:24 +0100

    chore: ...
    lnk to exe path and exe
    	repo
    
    vdesk path and exe
    
    chat gpt lnk home folder

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
@@ -14,8 +15,10 @@ func main() {
 
     lnkPath := os.Args[1]
 
-    // Use rundll32 to open the .lnk file
+    // Start the command in detached mode to avoid any subprocess blocking
     cmd := exec.Command("rundll32", "url.dll,FileProtocolHandler", lnkPath)
+    cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
+
     err := cmd.Start()
     if err != nil {
         fmt.Printf("Failed to open %s: %v\n", lnkPath, err)
@@ -23,4 +26,5 @@ func main() {
     }
 
     fmt.Println("Shortcut launched successfully")
+    os.Exit(0) // Explicit exit to prevent hanging
 }
